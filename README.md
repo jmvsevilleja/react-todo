d1bb21bd83a71ee57883634eb44b39d58a79a95c
Administrator <jessmark.sevilleja@bayviewtechnology.com>
2019-08-15 17:06:23 +0800

    Hello World

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react-todo
+
+https://levelup.gitconnected.com/react-typescript-with-webpack-2fceebb8faf
+
+
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const ROOT = document.querySelector(".container");
+ReactDOM.render(<h1>Hello World!</h1>, ROOT);
